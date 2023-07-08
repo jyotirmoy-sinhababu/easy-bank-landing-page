1c6731e2349b6e8ded65baa9acdb2c1ca783a9fe
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-07-08 13:00:40 +0530

    mobile-style added

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Logo from '../logo/Logo';
 import Btn from '../button/Btn';
 
 import './navStyle.css';
+import { RxHamburgerMenu } from 'react-icons/rx';
 
 const Nav = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  const handleClick = () => {
+    if (!isMobile) {
+      setIsMobile(true);
+    }
+    if (isMobile) {
+      setIsMobile(false);
+    }
+  };
   return (
     <div className='nav-cnt'>
       <div className='nav-logo-cnt'>
@@ -18,8 +29,28 @@ const Nav = () => {
         <button className='nav-btn'>Blog</button>
         <button className='nav-btn'>Careers</button>
       </div>
-      <div>
+      <div className='nav-req-btn-cnt'>
         <Btn />
+      </div>
+      <div className='mobile-view'>
+        <div className='ham-bur-btn-cnt'>
+          {' '}
+          <button onClick={handleClick} className='ham-bur-btn'>
+            <RxHamburgerMenu />
+          </button>
+        </div>
+
+        {!isMobile ? (
+          <div className='mobile-view-allBtn'>
+            <div className='nav-mobile-btn-cnt'>
+              <button className='nav-mobile-btn'>Home </button>
+              <button className='nav-mobile-btn'>About</button>
+              <button className='nav-mobile-btn'>Contact</button>
+              <button className='nav-mobile-btn'>Blog</button>
+              <button className='nav-mobile-btn'>Careers</button>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
