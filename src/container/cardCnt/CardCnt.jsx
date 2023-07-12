484f2b3f4c728e1cb0c7a533f86088bed66c96d2
jyotirmoy sinhababu <jyotirmoysinhababu@gmail.com>
2023-07-12 16:53:59 +0530

    style added

--- src/container/cardCnt/CardCnt.jsx
+++ src/container/cardCnt/CardCnt.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import './cardCntStyle.css';
 
 import Card from '../../components/card/Card';
 
 const CardCnt = () => {
   return (
-    <div>
-      <p>Latest Article</p>
-      <div>
+    <div className='cardComp-cnt'>
+      <div className='cardComp-para-cnt'>
+        <p className='cardComp-para'>Latest Article</p>
+      </div>
+      <div className='main-cnt'>
         <Card />
       </div>
     </div>
